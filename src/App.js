96197614b14ff2875fbcfed1d9df11d5affc5672
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2021-12-21 16:53:11 -0300

    se reemplaza CDN de bootstrap-icons por npm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-// import RouterApp from "./router/RouterApp";
+
 import Home from "./components/Home";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./sass/style.scss";
 
 const App = () => { 
   return (
     <div>    
-        {/* <RouterApp/>  */}
         <Home/>
     </div>
   );
